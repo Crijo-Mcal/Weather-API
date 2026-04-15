ecb7b3392c5fa15a2fa9a2459031d038ade6b816
Crijolise Morglia <crijolisemor25@gmail.com>
2026-04-15 14:18:34 +0100

    add update backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ import { setveValueOnRedis, getValueOnRedis, checkValueOnRedis } from "./redis/r
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 
 
